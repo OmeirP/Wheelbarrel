58f58fb9a475b2bc8e7a4bee99f6e1e843e49b8c
Omeir-CoolerMaster <50913215+OmeirP@users.noreply.github.com>
2020-03-06 20:16:44 +0000

    just fixing master before making pause menu branch

--- WheelBarrELGame.py
+++ WheelBarrELGame.py
@@ -71,8 +71,8 @@ highscore['highscorething']"""
 #    gameDisplay.blit(text, (0,0))
 
 
-def air():
-    gameDisplay.blit(winImg, (random.randrange(0, display_width),(random.randrange(0, display_width), 2, 2) ))
+#def air():
+#    gameDisplay.blit(winImg, (random.randrange(0, display_width),(random.randrange(0, display_width), 2, 2) ))
 
 
 def highscorereblit(dodged):

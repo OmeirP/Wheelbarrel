0b710d75965705371ce5a205bc1fad8d28ae211c
Omeir-CoolerMaster <50913215+OmeirP@users.noreply.github.com>
2020-05-31 00:21:37 +0100

    Almost added button, will relocate, give function and probably widen later.

--- WheelBarrELGame.py
+++ WheelBarrELGame.py
@@ -269,10 +269,12 @@ def market(): # trigger shop func through button added on menu in game_intro
                 quit()
         gameDisplay.blit(menuBackgroundImg, (0,0))
         largeText = pygame.font.Font('freesansbold.ttf', 88)
-        TextSurf, TextRect = set_colour("Shop", largeText)
+        TextSurf, TextRect = set_colour("Upgrades", largeText)
         TextRect.center = ((display_width/2), (display_height/2))
         gameDisplay.blit(TextSurf, TextRect)
 
+
+        button("Speed Upgrade",650,500,'x?','y?','colour1','colour2',"SpeUp")
         button("Exit",650,500,100,50,red,bright_red,"quit") # keep same
 
         mouse = pygame.mouse.get_pos()
